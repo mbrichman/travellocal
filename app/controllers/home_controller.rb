2ba491748b9819d988cd51b7cd04de7513928cf0
Mark Richman <mbrichman@yahoo.com>
2013-06-01 11:32:31 -0500

    login working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @city = City.new
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user.present? && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to root_url
     else
       render 'new'
     end
