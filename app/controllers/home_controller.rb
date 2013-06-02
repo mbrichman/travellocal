54237f47a0446cb4499fb7a546262d4768119b0b
Mark Richman <mbrichman@yahoo.com>
2013-06-02 15:01:37 -0500

    Changed cities dropdown. Fixed issue with home controller for non-logged in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    # @city = City.new
-    # p = Place.where("places.photo_file_name IS NOT NULL")
-    # @places = p[0..4]
+    @city = City.new
+    p = Place.where("places.photo_file_name IS NOT NULL")
+    @places = p[0..4]
   end
 end
