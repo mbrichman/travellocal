b3a070f2997c4d72d3be0d07e026dd6ac1bd9c7e
Mark Richman <mbrichman@yahoo.com>
2013-06-02 12:07:20 -0500

    mark changes

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,10 +2,10 @@ class City < ActiveRecord::Base
   attr_accessible :name
   has_many :places
 
-  before_save :downcase_form_iput
+  before_save :downcase_form_input
 
   def downcase_form_input
-    name = name.downcase
+    self.name = self.name.downcase
   end
 
 end
