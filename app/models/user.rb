d44e849cd31199340f7032ad7c4e32956940bfa6
Mark Richman <mbrichman@yahoo.com>
2013-06-06 22:44:45 -0500

    new method for user network

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :name, :email, :password, :password_confirmation, :city_id
 
-  validates :name, :email, presence: true
-  validates :email, uniqueness: true
+  validates   :name, :email, presence: true
+  validates   :email, uniqueness: true
 
-  has_many :connections, :dependent => :destroy
-  has_many :reviews
-  has_many :trips, :dependent => :destroy
-  has_many :favorites, :dependent => :destroy
-  has_many :authentications
+  has_many    :connections, :dependent => :destroy
+  has_many    :reviews
+  has_many    :trips, :dependent => :destroy
+  has_many    :favorites, :dependent => :destroy
+  has_many    :authentications
   belongs_to  :city
-  has_many :wishlists, :dependent => :destroy
+  has_many    :wishlists, :dependent => :destroy
 
   before_save :downcase_input
 
@@ -24,12 +24,12 @@ class User < ActiveRecord::Base
   end
 
   def friends
-    self.connections.map {|friend| friend.user_connection}
+    self.nth_level(1)
   end
 
   def friends_reviews
     # @reviews = self.nth_level(1).map {|user| user.reviews.map {|review| review.place}}.flatten.uniq{|place| place.id}
-    friend_ids = self.nth_level(1).map {|u| u.id }
+    friend_ids = friends.map {|u| u.id }
     @user_reviews = Review.where(:user_id => friend_ids).group_by(&:place_id)
 
   end
@@ -59,11 +59,35 @@ class User < ActiveRecord::Base
         nth_level(level, array, c.user_connection.connections)
       end
     end
-    return array
+    return array.uniq
   end
 
   def get_connections_by_level(level)
     self.nth_level(level).map {|user| user.name.capitalize}.join(' ')
+  end
+
+  def clean_connections
+    network = Array.new
+    two = Array.new
+    three = Array.new
+    self.connections.each do |first|
+      network << {1 => first.user_connection }
+      first.user_connection.connections.each do |second|
+        network << {2 => second.user_connection}
+        second.user_connection.connections.each do |third|
+          network << {3 => third.user_connection}
+        end
+      end
+    end
+    level1 = network.select {|n| n[1]}.uniq.map{|n| n[1]}
+    level2 = network.select {|n| n[2]}.uniq.map{|n| n[2]}
+    level3 = network.select {|n| n[3]}.uniq.map{|n| n[3]}
+    level2.reject! {|n| level1.include?(n)}
+    level2.reject! {|n| self.name == n}
+    level3.reject! {|n| level2.include?(n)}
+    level3.reject! {|n| level1.include?(n)}
+    level3.reject! {|n| self.name == n}
+    @users = [{:level1 => level1 }, {:level2 => level2}, {:level3 => level3}]
   end
 
 end
