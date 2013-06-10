d79d80878c49a52292ff505123224f12b307d2f1
Mark Richman <mbrichman@yahoo.com>
2013-06-10 17:15:27 -0500

    fix stupid typo

--- app/models/recommendation_request.rb
+++ app/models/recommendation_request.rb
@@ -1,6 +1,6 @@
 class RecommendationRequest < ActiveRecord::Base
   attr_accessible :city_id, :description, :user_id, :friend_id
-  has_many :recommendations, class_name: 'Recommendation', foreign_key: 'rr_id', dependant: :destroy
+  has_many :recommendations, class_name: 'Recommendation', foreign_key: 'rr_id', dependent: :destroy
   belongs_to :user
   belongs_to :city
 end
